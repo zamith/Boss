53ef9c2ccaa0d0d7b50e90ef0527ab0621a24be7
Zamith <zamith.28@gmail.com>
2012-08-10 18:03:47 +0100

    Correcting migrations

--- spec/dummy/db/migrate/20120810170148_change_start_date_to_published_date.boss.rb
+++ spec/dummy/db/migrate/20120810170148_change_start_date_to_published_date.boss.rb
@@ -1,3 +1,4 @@
+# This migration comes from boss (originally 20120810170007)
 class ChangeStartDateToPublishedDate < ActiveRecord::Migration
   def change
     rename_column :boss_posts, :start_date, :published_date
