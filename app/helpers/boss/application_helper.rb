1a9e7c78dea33002030b5e33797e2da5a399b062
Zamith <zamith.28@gmail.com>
2012-08-15 18:33:14 +0100

    Last tests and touches on banner viewer

--- app/helpers/boss/application_helper.rb
+++ app/helpers/boss/application_helper.rb
@@ -1,7 +1,11 @@
 module Boss
   module ApplicationHelper
+    def ac_helper
+      @ac_helper ||= ActionController::Base.new
+    end
+
     def banner_viewer
-      ActionController::Base.new.render_to_string(partial: "boss/banners/banner_viewer", locals: {banners: Boss::Banner.all}).html_safe
+      ac_helper.render_to_string(partial: "boss/banners/banner_viewer", locals: {banners: Boss::Banner.all}).html_safe
     end
   end
 end
---
+++ spec/dummy/spec/helpers/boss_application_helper_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper.rb'
+
+describe Boss::ApplicationHelper do
+set_fixture_class :boss_banners => 'Boss::Banner'
+fixtures :boss_banners
+
+let(:banner) { boss_banners(:gb) }
+
+  context "banner viewer" do
+    it "should return a string with the html for all the banners" do
+      Boss::Resource.create_image fixture_file_upload('/files/computer_0050.jpg', 'image/jpeg')
+      Boss::Banner.should_receive(:all).and_return([banner])
+      helper.banner_viewer.should == helper.ac_helper.render_to_string(partial: "boss/banners/banner_viewer", locals:{banners: [banner]})
+    end
+  end
+end
