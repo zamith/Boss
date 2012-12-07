9791065a381578707cd4d99b9aaee7cc619b1d06
Zamith <zamith.28@gmail.com>
2012-12-07 16:23:49 +0000

    Not showing draft posts

--- app/controllers/boss/posts_controller.rb
+++ app/controllers/boss/posts_controller.rb
@@ -2,7 +2,7 @@ class Boss::PostsController < Boss::ApplicationController
   load_and_authorize_resource :class => "Boss::Post"
 
   def index
-    @posts = Boss::Post.posts_for_index
+    @posts = Boss::Post.posts_for_index conditions: { draft: false }
   end
 
   def show
