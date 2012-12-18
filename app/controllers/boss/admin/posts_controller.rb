315c117840cd15ac380765370d5bd64fcda8fffb
Zamith <zamith.28@gmail.com>
2012-12-18 19:07:38 +0000

    Oh god I'm sleeping

--- app/controllers/boss/admin/posts_controller.rb
+++ app/controllers/boss/admin/posts_controller.rb
@@ -26,7 +26,7 @@ class Boss::Admin::PostsController < Boss::Admin::ApplicationController
       flash[:error] = t('posts.flash.failed_to_destroy')
     end
 
-    redirect_to admin_posts_index
+    redirect_to admin_posts_index_path
   end
 
   def save
